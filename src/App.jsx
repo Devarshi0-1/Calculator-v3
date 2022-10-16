ad9756c38caeef305ebe75645d9dc1f8061678de
Devarshi <devarshidwi@gmail.com>
2022-10-17 01:14:16 +0530

    Added React Logo with Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Calculator from './Components/Calculator'
+import reactLogo from './assets/react.svg'
 export default function App() {
 
   return (
     <div className="container">
       <Calculator />
+      <img src={reactLogo} alt="" />
     </div>
   )
 }
